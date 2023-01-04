bbb52a1f5c3c4d6ac41da92d95cba4be62437299
Ariethel <a.renzullo9@studenti.unisa.it>
2023-01-04 16:13:33 +0100

    aggiunte diverse entita' al codice e tabelle al DB

--- HappyFields/DBHappyFields.sql
+++ HappyFields/DBHappyFields.sql
@@ -30,3 +30,37 @@ create table gestore(
 create table sport(
 	nome_s varchar(255) primary key
 );
+
+create table evento(
+	titolo varchar(255) not null,
+    sport varchar(255) references sport.nome_s,
+    #luogo calcolabile da campo
+    campo varchar(255) references campo.nome_c,
+    data date not null,
+    ora double not null,
+    primary key (titolo, campo)
+);
+
+create table campo(
+	nome_c varchar(255) primary key,
+    numero_giocatori int not null,
+    provincia varchar(255) not null,
+    citta varchar(255) not null,
+    via varchar(255) not null,
+    costo double not null,
+    lunghezza double not null,
+    larghezza double not null
+);
+
+create table prenotazione(
+	numero_prenotazione int primary key auto_increment,
+    admin_prenotazione varchar(255) references utente.username,
+    evento varchar(255) references evento.titolo
+);
+
+create table gestisce(
+	gestore varchar(255) references gestore.username_g,
+    campo varchar(255) references campo.nome_c,
+    primary key (gestore, campo)
+);
+

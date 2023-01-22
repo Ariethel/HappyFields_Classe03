c2ea4b32a84f57fd598fb784c5faa5e46b10edf4
annachiara01 <106352008+annachiara01@users.noreply.github.com>
2023-01-22 13:45:45 +0100

    Add files via upload

---
+++ HappyFields/DBInsertHappyFields.sql
@@ -0,0 +1,135 @@
+drop schema if exists HappyFields;
+create schema HappyFields;
+use HappyFields;
+create table utente(
+	username varchar(255) not null,
+    pass varchar(255) not null,
+    provincia varchar(2) not null,
+    citta varchar(255) not null,
+    via varchar(255) not null,
+    telefono long not null,
+    iban varchar(27),
+    preferenza varchar(255) references Sport.nome_s,
+    primary key(username, pass)
+);
+
+create table gestore(
+	username_g varchar(255) not null,
+    pass_g varchar(255) not null,
+    nome varchar(255) not null,
+    cognome varchar(255) not null,
+    provincia varchar(255) not null,
+    citta varchar(255) not null,
+    via varchar(255) not null,
+    iban_g varchar(255) not null,
+    telefono_g varchar(255) not null,
+    primary key (username_g, pass_g)
+    # vanno aggiunti i campi ma devo pensare a come fare
+);
+
+create table sport(
+	nome_s varchar(255) primary key
+);
+
+create table evento(
+	titolo varchar(255) not null,
+    sport varchar(255) references sport.nome_s,
+    #luogo calcolabile da campo
+    campo varchar(255) references campo.nome_c,
+    data_e date not null,
+    ora double not null,
+    primary key (titolo, campo)
+);
+
+create table campo(
+	nome_c varchar(255) primary key,
+    numero_giocatori int not null,
+    provincia varchar(255) not null,
+    citta varchar(255) not null,
+    via varchar(255) not null,
+    costo double not null,
+    lunghezza double not null,
+    larghezza double not null
+);
+
+create table prenotazione(
+	numero_prenotazione int primary key auto_increment,
+    admin_prenotazione varchar(255) references utente.username,
+    evento varchar(255) references evento.titolo
+);
+
+create table gestisce(
+	gestore varchar(255) references gestore.username_g,
+    campo varchar(255) references campo.nome_c,
+    primary key (gestore, campo)
+);
+
+
+
+insert into utente values
+('annac01', 'Annachiara', 'NA', 'saviano', 'v.gemito', 3289018431, 'IT99C1234567689012345678902', 'calcio'),
+('genn09', 'calcare00', 'NA', 'san paolo', 'vincenzo gemito', 3289876431, 'IT99C1987654329012345678902', 'tennis'),
+('mariolino', 'bradipi99', 'SA', 'mercato san severino', 'cirtcumvallazione', 3299918431, 'IT89S1234567689012345678902', 'basket'),
+('armandino88', 'patatiness', 'SA', 'baronissi', 'vittorio emanuele', 3275432201, 'IT01C789654389012345678902', 'calcio'),
+('cucciolone70', 'mammaaaaaaaa', 'SA', 'baronissi', 'vittorio emanuele', 3256784431, 'IT01C9987765543321166778654', 'paddle'),
+('armandino88', 'giornataccia', 'AV', 'avellino', 'san sebastiano', 3456789011, 'IT01C000099889012345678902', 'pallavolo'),
+('beatrice69', 'bestiolinabbb', 'SA', 'baronissi', 'vittorio emanuele', 3987654321, 'IT01C789654389010000000099', 'tennis'),
+('carlino01', 'occhialidasole', 'SA', 'mercato s.severino', 'circumvallazione', 3289007864, 'IT01C789654389019090909090', 'calcetto'),
+('matti01', 'monellone', 'SA', 'baronissi', 'vittorio emanuele', 3289099999, 'IT01S709876767662345678902', 'tennis'),
+('antoLeader', 'capitanoccc', 'NA', 'san paolo', 'feudo', 3256787766, 'IT09T4567665433789099887767', 'pallavolo'),
+('barbarella', 'barbie00', 'SA', 'baronissi', 'vittorio emanuele', 3099998883, 'IT02G789658888882340098772', 'paddle');
+
+insert into gestore values
+('gennaro99','gennaro','scognamiglio', 'zucchineggg', 'SA', 'baronissi', 'vittorio veneto', 'IT96Q2266789876543668890022', 3289095543),
+('carlo97','carlo','tammarro', 'sonosolare', 'SA', 'mercato san severino', 'curcumvallazione', 'IT97T0066789876543668890022',3667895543),
+('giulio99','giulio','incoronato', 'studiostudio', 'NA', 'piazzolla', 'antonio ciccone', 'IT99G6699009988776543780976', 3289876654),
+('antonio66','antonio','scognamiglio', 'ooiiuuyytt', 'AV', 'aversa', 'antonio ciccone', 'IT96Q2266789876543668890022', 3289095543),
+('marioM', 'mario', 'marrone', 'uiuiuiui', 'NA', 'poimigliano d\'arco', 'via delle carote', 'IT96Q1111112255543668890022', 3278906654),
+('ernestoT', 'ernesto', 'tafuro', 'uiooooui', 'SA', 'nocera inferiore', 'via giuseppe mazzini', 'IT96Q1199885655543042890022', 3098201654),
+('ernesto00', 'ernesto', 'castaldo', 'ooooui', 'SA', 'baronissi', 'via degli ulivi' ,'IT96Q1199885655830570943722', 3087689054);
+
+insert into sport values
+('calcio'),
+('tennis'),
+('pallavolo'),
+('basket'),
+('calcetto'),
+('paddle');
+
+insert into evento values
+('partitina', 'calcio', 'pollicino', '2023-02-19', 18.00),
+('partita.tennis', 'tennis', 'palla d\'oro', '2023-03-18', 15.00),
+('giochiamoInsieme', 'pallavolo', 'bombonera', '2023-03-20', 15.00),
+('amanti del basket', 'basket', 'campetti nike','2023-04-21',17.30),
+('calciando', 'calcetto', 'real football', '023-03-22', 18.55),
+('paddleLove', 'paddle', 'blue paddle club','2023-04-01',15.00),
+('pallavolo-partita', 'pallavolo', 'sporting club','2023-03-24',20.00);
+
+insert into campo values
+('pollicino', 22, 'NA', 'saviao', 'v.gemito', 100.00, 100.00, 65.50 ),
+('palla d\oro',2 , 'SA', 'Baronissi', 'delle castagne', 15.00, 23.00, 10.15),
+('bombonera', 12, 'AV', 'avella', 'degli ulivi',60.00 ,18.00 ,9.00 ),
+('campetti nike',10 , 'SA', 'nocera inferore', 'via dei pomodori', 50.00, 28.00, 15.00),
+('real football', 10, 'SA', 'mercato sanseverino', 'via della fontana',50.00 ,35.00 , 20.00 ),
+('blue paddle club', 4, 'SA', 'baronissi', 'via genovesi', 40.00, 20.00, 10.00),
+('sporting club', 12, 'NA', 'saviano', 'antonio ciccone', 70.00 ,20.00 , 11.00 );
+
+insert into prenotazione values
+(1, 'annachiara01', 'partitina'),
+(2, 'genn09', 'partita.tennis'),
+(3, 'armandino88', 'giochiamoInsieme'),
+(4, 'mariolino', 'amanti del basket'),
+(5, 'carlino01', 'calciando'),
+(6, 'antoLeader', 'pallavolo-partita'),
+(7,'cucciolone70', 'paddleLove' );
+
+insert into gestisce values
+('gennaro99','campetti nike'),
+('carlo97', 'real football'),
+('giulio99', 'pollicino'),
+('antonio66', 'bombonera'),
+('marioM', 'sporting club'),
+('ernesto00','blue paddle club'),
+('ernestoT','palla d\oro');
+
+
